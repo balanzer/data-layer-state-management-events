b84f1d7a69dc6721d8ebe1ba8396b4c645fd02a1
Murali Varath <muralitharan.balanzer@gmail.com>
2025-04-15 14:21:18 -0400

    save changes

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,15 +1,9 @@
 import { Logger } from "./logger/log";
 import { AppTasks } from "./app-flow/app-task";
 
-import logAppEvent from "./data/events/app-events";
-import logDataEvent from "./data/events/data-events";
-
 const logger = new Logger("main-app");
 
 //INIT Todo tasks app
 const appTasks: AppTasks = new AppTasks();
 appTasks.setupListners();
 appTasks.addDummyTasks();
-
-logAppEvent("app loaded");
-logDataEvent("app loaded");
